9f613fbae0ec62f827c59fa5661a3f1c731ac146
unknown <gustavo-oliveira-macedo@hotmail.com>
2020-05-20 17:46:28 -0300

    ./

--- src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
+++ src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'dio-cadastro-filmes',
@@ -8,15 +8,23 @@ import {FormBuilder, FormGroup} from '@angular/forms';
 })
 export class CadastroFilmesComponent implements OnInit {
 
-  options: FormGroup;
+  cadastro: FormGroup;
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
 
-    this.options = this.fb.group({
-      hideRequired: false,
-      floatLabel: 'auto',
+    this.cadastro = this.fb.group({
+      titulo: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(256)]],
+      urlFoto: ['', [Validators.minLength(10)]],
+      dtLancamento: ['', [Validators.required]],
+      descricao: ['',[Validators.required, Validators.maxLength(256)]],
+      nota: [0, [ Validators.required, Validators.min(0), Validators.max(0)]],
+      urlIMDb: ['',[Validators.maxLength(10)]],
+      genero: ['',[Validators.required] ]
+
+
+
     });
 
   }
