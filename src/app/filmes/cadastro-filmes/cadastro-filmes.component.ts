1ad126fed9341fdc9e9cef49960dbd1a0341decd
unknown <gustavo-oliveira-macedo@hotmail.com>
2020-05-21 18:11:02 -0300

    dashed

--- src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
+++ src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+
 @Component({
   selector: 'dio-cadastro-filmes',
   templateUrl: './cadastro-filmes.component.html',
@@ -12,21 +13,32 @@ export class CadastroFilmesComponent implements OnInit {
 
   constructor(private fb: FormBuilder) { }
 
-  ngOnInit() {
+  get f() {
+      return this.cadastro.controls;
+  }
+
+  ngOnInit(): void {
 
     this.cadastro = this.fb.group({
       titulo: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(256)]],
       urlFoto: ['', [Validators.minLength(10)]],
       dtLancamento: ['', [Validators.required]],
-      descricao: ['',[Validators.required, Validators.maxLength(256)]],
-      nota: [0, [ Validators.required, Validators.min(0), Validators.max(0)]],
-      urlIMDb: ['',[Validators.maxLength(10)]],
-      genero: ['',[Validators.required] ]
-
-
-
+      descricao: ['', [Validators.minLength(10)]],
+      nota: [0, [Validators.required, Validators.min(0.1), Validators.max(10)]],
+      urlIMDb: ['', [Validators.minLength(10)]],
+      genero: ['', [Validators.required]]
     });
-
   }
+
+  salvar(): void {
+    if (this.cadastro.invalid) {
+      return;
+    }
+    alert('SUCESSO!!\n\n' + JSON.stringify(this.cadastro.value, null, 4));
+  }
+  reiniciarForm(): void{
+    this.cadastro.reset();
+  }
+  
 
 }
